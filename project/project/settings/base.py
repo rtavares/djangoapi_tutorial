ede784351ab480c3b63e275ac8e5411b6de41673
Raul Tavares <rtavares@gmail>
2020-04-18 19:06:06 +0100

    Adding DRF permissions settings.

--- project/project/settings/base.py
+++ project/project/settings/base.py
@@ -161,5 +161,10 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 20
+    'PAGE_SIZE': 20,
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+      'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
 }
